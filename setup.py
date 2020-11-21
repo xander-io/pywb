4111ae732206d3160a3e4cb7c28792864a920766
Zack Anderson <ztan@Zacks-MacBook-Pro.local>
2020-11-20 22:59:04 -0500

    Update setup.py to build wheel

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 import setuptools
+
 from pyww import VERSION
+
+with open("requirements.txt", "r") as reqs_file:
+    reqs = [i.strip() for i in reqs_file]
 
 setuptools.setup(
     name="pyww",
     version=VERSION,
     author="xander-io",
-    author_email="zackanderson026@gmail.com",
-    description="Web watcher for python",
+    description="Python Web Watcher",
     packages=setuptools.find_packages(),
-    python_requires=">=3.6",
-    entry_points={ "console_scripts": ["pyww = pyww.app:run"] }
+    install_requires=reqs,
+    python_requires=">=3.9.0",
+    entry_points={"console_scripts": ["pyww = pyww.app:run"]}
 )
