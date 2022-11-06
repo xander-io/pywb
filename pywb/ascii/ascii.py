4b3ef31bd860ac3c4e93b1db85d974633b110507
xander-io <xander-io>
2022-11-06 11:48:31 -0500

    Generate ascii from files

---
+++ pywb/ascii/ascii.py
@@ -0,0 +1,25 @@
+from os import path, walk
+from random import randint
+
+from pywb import VERSION
+
+
+def read_ascii():
+    ascii_path = generate_ascii_path()
+
+    with open(ascii_path, "r", encoding="utf-8") as f:
+        ascii = f.read()
+    return ascii
+
+def generate_ascii_path():
+    ascii_folder = path.join(path.dirname(__file__), "banners")
+    # Get filenames from ascii path
+    fnames = next(walk(ascii_folder))[2]
+    fname = fnames[randint(0, len(fnames) - 1)]
+    # Exclude any python files
+    while (path.splitext(fname)[1] == ".py"):
+        fname = fnames[randint(0, len(fnames) - 1)]
+    return path.join(ascii_folder, fname)
+
+def generate_ascii_art():
+    return read_ascii() % VERSION
