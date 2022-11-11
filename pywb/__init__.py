a847da001998d8cef4c1552ae76cd8e21320100e
xander-io <xander-io>
2022-11-11 14:04:30 -0500

    log dir can be changed via env and settings

--- pywb/__init__.py
+++ pywb/__init__.py
@@ -4,3 +4,4 @@ Pyww module variables for versioning and misc env keys
 
 VERSION = "0.0.1a0"
 ENVIRON_DEBUG_KEY = "PYWB_DEBUG"
+ENVIRON_LOG_DIR_KEY = "PYWB_LOG_DIR"
--- pywb/core/logger.py
+++ pywb/core/logger.py
@@ -1,21 +1,34 @@
 import logging
-from os import environ
+from os import environ, makedirs, path
 
-from pywb import ENVIRON_DEBUG_KEY
+from pywb import ENVIRON_DEBUG_KEY, ENVIRON_LOG_DIR_KEY
+
+# Log dir changes if environment var is set
+LOG_DIR = "logs" if ENVIRON_LOG_DIR_KEY not in environ else environ[ENVIRON_LOG_DIR_KEY]
+DEFAULT_LOG_PATH = path.join(LOG_DIR, "pywb.out")
 
 
-def _init_logging():
+def _init_logging(output_path=DEFAULT_LOG_PATH):
+    makedirs(path.dirname(output_path), exist_ok=True)
     formatter = logging.Formatter(
         "[%(asctime)s - %(levelname)s] %(message)s")
 
-    console_logger = logging.getLogger(__name__)
-    console_logger.setLevel(logging.DEBUG
-                            if ENVIRON_DEBUG_KEY in environ else logging.INFO)
+    f_logger = logging.getLogger(__name__)
+    for hdlr in f_logger.handlers[:]:  # remove all old handlers
+        f_logger.removeHandler(hdlr)
 
-    stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(formatter)
-    console_logger.addHandler(stream_handler)
-    return console_logger
+    if f_logger.getEffectiveLevel() != logging.DEBUG:
+        f_logger.setLevel(logging.DEBUG
+                          if ENVIRON_DEBUG_KEY in environ else logging.INFO)
+
+    f_handler = logging.FileHandler(output_path, "a")
+    f_handler.setFormatter(formatter)
+    f_logger.addHandler(f_handler)
+    return f_logger
+
+
+def set_logger_output_path(output_path):
+    _init_logging(output_path=output_path)
 
 
 logger = _init_logging()
