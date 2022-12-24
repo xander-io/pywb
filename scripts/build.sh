5520c6c86bc143eaf4182b4af4cc6993e11a4c13
xander-io <xander-io>
2022-12-24 10:03:22 -0500

    Added docker build steps to linux env

--- scripts/build.sh
+++ scripts/build.sh
@@ -19,4 +19,15 @@ docker save pywb:latest -o ./dist/pywb_docker.tar
 echo "Compressing as a tgz..."
 gzip -9c ./dist/pywb_docker.tar > ./dist/pywb_docker.tgz
 rm ./dist/pywb_docker.tar
+
+echo "Setting up release folder structure and moving files..."
+mkdir ./dist/docker-windows
+mkdir ./dist/docker-mac-linux
+mkdir  ./dist/python
+cp ./dist/pywb_docker.tgz ./dist/docker-windows/
+mv ./dist/pywb_docker.tgz ./dist/docker-mac-linux/
+mv ./dist/$PYWB_WHEEL_NAME ./dist/python/
+cp ./scripts/docker/windows/* ./dist/docker-windows/
+cp ./scripts/docker/mac-linux/* ./dist/docker-mac-linux/
+cp ./README.md ./dist/
 echo "DONE!"
