df48788590db666b30dd929307542c363680023d
Xander <xander-io>
2022-12-24 22:31:23 -0500

    mac notifications use pync

--- pywb/core/notify/local_notifier.py
+++ pywb/core/notify/local_notifier.py
@@ -1,41 +1,30 @@
 from platform import system
-
-try:
-    # Windows?
-    from win11toast import notify
-except ImportError:
-    # Maybe Mac OSX?
-    try:
-        from mac_notifications import client
-    except ImportError:
-        # Definitely Linux
-        pass
-
 from webbrowser import open
 
 from pywb.core.notify.notifier import Notifier
+
+_OS_NAME = system()
+if _OS_NAME == "Windows":
+    from win11toast import notify as win_notify
+elif _OS_NAME == "Darwin":
+    from pync import notify as mac_notify
 
 
 class LocalNotifier(Notifier):
     def __init__(self) -> None:
         super().__init__()
 
-    def notify(self, title: str, msg: str, url: str = None) -> None:
-        os_name = system()
-        if os_name == "Windows":
+    def notify(self, title: str, msg: str, url: str = "") -> None:
+        global _OS_NAME
+        if _OS_NAME == "Windows":
             self.__notify_windows_os(title, msg, url=url)
-        elif os_name == "Darwin":
+        elif _OS_NAME == "Darwin":
             self.__notify_mac_os(title, msg, url=url)
 
-    def __notify_mac_os(self, title: str, msg: str, url: str = None) -> None:
-        client.create_notification(
-            title=title,
-            subtitle=msg,
-            action_button_str="Open URL",
-            action_callback=lambda: open(url, new=2)
-        )
+    def __notify_mac_os(self, title: str, msg: str, url: str = "") -> None:
+        mac_notify(msg, title=title, open=url)
 
-    def __notify_windows_os(self, title: str, msg: str, url: str = None) -> None:
+    def __notify_windows_os(self, title: str, msg: str, url: str = "") -> None:
         button = None if not url else {
             'activationType': 'protocol', 'arguments': url, 'content': "Open URL"}
-        notify(title, msg, button=button, on_click=url)
+        win_notify(title, msg, button=button, on_click=url)
